494edf5600cb8e4bfd7b9974f6522e12bbdcff90
SungJong Park <zzxxx1@naver.com>
2024-01-09 22:48:45 +0900

    v4

--- src/market/index.js
+++ src/market/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import io from 'socket.io-client'
 import axios from 'axios';
 import './index.css';
 
@@ -6,50 +7,28 @@ const LiveChart = () => {
   const [coin, setCoin] = useState();
 
   useEffect(() => {
-    // axios
-    //   .request({
-    //     method: 'GET',
-    //     url: 'https://api.upbit.com/v1/market/all?isDetails=false',
-    //     headers: {accept: 'application/json'}
-    //   })
-    //   .then((res) => {
-    //     console.log(res.data);
-    //     setCoin(res.data);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   })
 
-    const socket = new WebSocket('wss://api.upbit.com/websocket/v1')
+    const socket = io('http://localhost:3001');
+    socket.emit('getCoinData', ['KRW-BTC', 'KRW-ETH', 'KRW-XRP']);
 
-    socket.addEventListener("open", () => {
-      console.log('소켓 연결');
-      socket.send('[{"ticket":"test example"},{"type":"ticker", "codes":["KRW-BTC"]}, {"format":"DEFAULT"}]');
-    })
+    socket.on('coinData', (data) => {
+      console.log('Received coin data : ', data);
+    });
 
-    socket.addEventListener("message", (data) => {
-      console.log('데이터', data.data);
-    })
-
-    socket.addEventListener("close", () => {
-      console.log('소켓 종료');
-    })
-  })
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+  
   return (
     <div className="livechart-wrapper">
-      {/* <table>
-        <tbody>
-          {coin ? coin.map( c => {
-            return(
-              <tr>
-                <td>{c.market}</td>
-                <td>{c.korean_name}</td>
-                <td>{c.english_name}</td>
-              </tr>
-            )
-          }) : null}
-        </tbody>
-      </table> */}
+       <ul>
+        {/* {coin.map((coin) => (
+          <li key={coin.market}>
+            {coin.market} - {coin.trade_price}
+          </li>
+        ))} */}
+      </ul>
     </div>
   )
 }
