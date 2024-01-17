bf0c7bf8e4b69235bf176f1cfe8a75d612fa6bc1
iiww_B <iiww123@naver.com>
2024-01-17 17:42:50 +0900

    노트북 반응형 그리드, 메인2 전체틀 수정

---
+++ src/main/components/main2/components/chart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './chart.css';
+
+export default function Chart() {
+
+  return(
+    <div id="chart-box">
+      <div id="chart-top-box">
+        
+      </div>
+      <div id="chart-bottom-box">
+      <p>
+        <span className='chart-bottom-logo'>쩐의 전쟁 </span>
+          모의투자 게임에 참여하여 차트를 분석하고<br/>
+          미래를 예측하여 투자의 재미와 도전을 경험하세요
+      </p>
+      </div>
+    </div>
+  );
+}
